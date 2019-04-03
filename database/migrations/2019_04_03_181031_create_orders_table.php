5e0dc42aff80484d1a43a282dcb898a86dcd0f8f
ruhincse <ruhinm3@gmail.com>
2019-04-04 00:16:23 +0600

    order Table Added

---
+++ database/migrations/2019_04_03_181031_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('customer_id');
+            $table->string('order_date');
+            $table->string('order_status');
+            $table->string('total_products');
+            $table->string('sub_total');
+            $table->string('payment_status');
+            $table->string('pay');
+            $table->string('due');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
